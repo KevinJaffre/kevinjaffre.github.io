b8aedf01956bf747233a41f4d2b3e6e6706fba5b
Kevin <kevin56620@gmail.com>
2016-05-22 12:41:18 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# kevinjaffre.github.io" 
---
+++ js/app.js
@@ -0,0 +1,8 @@
+(function($){
+
+  $('#toggleMenu').click(function(e){
+    e.preventDefault();
+    $('nav').toggleClass('nav--active');
+  })
+
+})(jQuery);
